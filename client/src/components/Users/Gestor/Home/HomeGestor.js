2429b8091c7755186c77b3c5213ca12cbd648a79
João Barreira <jpiresbarreira@gmail.com>
2019-06-28 16:14:12 +0100

    Primeira gestão rotas

---
+++ client/src/components/Users/Gestor/Home/HomeGestor.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class HomeGestor extends React.Component {
+
+    state = {
+
+    };
+
+    render() {
+        return (
+            <div>
+                Home Gestor
+            </div>
+        );
+    }
+}
+
+export default HomeGestor;
---
+++ client/src/components/Users/UtilizadorCPDR/Home/HomeUtilizadorCPDR.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class HomeUtilizadorCPDR extends React.Component {
+
+    state = {
+
+    };
+
+    render() {
+        return (
+            <div>
+                Home UtilizadorCPDR
+            </div>
+        );
+    }
+}
+
+export default HomeUtilizadorCPDR;
