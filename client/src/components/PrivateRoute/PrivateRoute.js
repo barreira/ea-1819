a00e802d1f977575febd33b04907e3549568a964
João Barreira <jpiresbarreira@gmail.com>
2019-06-28 16:13:28 +0100

    Primeira gestão rotas

--- client/src/components/PrivateRoute/PrivateRoute.js
+++ client/src/components/PrivateRoute/PrivateRoute.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
+
 import UserHandler from '../../utils/userHandler';
 import HomeUtilizador from '../../components/Users/Utilizador/Home/HomeUtilizador';
 import HomeVisitante from '../../components/Users/Visitante/Home/HomeVisitante';
+import HomeUtilizadorCPDR from "../Users/UtilizadorCPDR/Home/HomeUtilizadorCPDR";
+import HomeGestor from "../Users/Gestor/Home/HomeGestor";
+import HomeAdministrador from "../Users/Administrador/Home/HomeAdministrador";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
 
@@ -14,13 +18,13 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
                 pathname: '/login',
                 state: { from: props.location }
             }} />
-        )} />)
+        )} />);
 
-    if (rest.testFunction)
+    if (rest.testFunction) {
         rest.testFunction();
+    }
 
     let pathLimitations = [];
-
 
     // FUNCIONAMENTO
     // Para cada tipo de utilizador definem-se as limitações dos paths
@@ -28,20 +32,20 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
 
     switch (typeUser) {
         case 'Visitante':
-
-
-            pathLimitations = ['/espaco']
+            pathLimitations = ['/', '/login', '/registar', '/espacos'];
 
             if (rest.path === '/') {
                 return (
                     <Route {...rest} render={(props) => (
                         <HomeVisitante {...props} />
-                    )} />
+                    )}/>
                 )
             }
+
             break;
 
         case 'Utilizador':
+            pathLimitations = ['/', '/eventos', '/notificacoes'];
 
             if (rest.path === '/') {
                 return (
@@ -54,24 +58,56 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
             break;
 
         case 'UtilizadorCPDR':
+            pathLimitations = ['/', '/espacos', '/eventos', '/pedidos', '/notificacoes'];
+
+            if (rest.path === '/') {
+                return (
+                    <Route {...rest} render={(props) => (
+                        <HomeUtilizadorCPDR {...props} />
+                    )}/>
+                )
+            }
 
             break;
 
-        case ' Gestor':
+        case 'Gestor':
+            pathLimitations = ['/', '/espacos', '/eventos', '/eventos/criar', '/eventos/modificar', '/espacos-comuns',
+                               '/espacos-comuns/criar', '/espacos-comuns/modificar', '/pedidos', '/notificacoes'];
+
+            if (rest.path === '/') {
+                return (
+                    <Route {...rest} render={(props) => (
+                        <HomeGestor {...props} />
+                    )}/>
+                )
+            }
+
             break;
 
         case 'Administrador':
+            pathLimitations = ['/', '/espacos', '/espacos/carregar', '/eventos', '/eventos/carregar', '/utilizadores',
+                               'utilizadores/carregar', '/espacos-comuns'];
+
+            if (rest.path === '/') {
+                return (
+                    <Route {...rest} render={(props) => (
+                        <HomeAdministrador {...props} />
+                    )}/>
+                )
+            }
+
             break;
     }
 
-    if (pathLimitations.includes(rest.path))
-        return (redirectLogin)
+    if (!pathLimitations.includes(rest.path)) {
+        return (redirectLogin);
+    }
 
     return (
         <Route {...rest} render={(props) => (
             <Component {...props} />
         )} />
     )
-}
+};
 
 export default PrivateRoute;
